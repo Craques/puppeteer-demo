a494d5b067d2daebf1f1b1d3470a11a2677ee731
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-22 16:07:53 +0200

    added functionality to paginate and collect titles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+let pageTitles = [];
 
 (async()=>{
     const browser = await puppeteer.launch({
@@ -8,12 +9,29 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage()
     await page.goto('https://news.ycombinator.com/')
     await page.waitFor('td')
+    //Get first page data
     const stories = await page.evaluate(()=>{
         return Array.from(document.querySelectorAll('td.title:not([align="right"])')).map((title)=>{
             return title.querySelector('a').innerText
         })
     })
 
-    console.log(stories)
-    browser.close()
+    const moreLink = await page.evaluate(()=>document.querySelector('.moreLink'))
+    let n = 1
+    while(moreLink && n < 10){
+        //navigate to the subsequent pages getting the information
+        await page.goto(`https://news.ycombinator.com/news?p=${n}`)
+        await page.waitFor('td')
+        //Get first page data
+        const stories = await page.evaluate(()=>{
+            return Array.from(document.querySelectorAll('td.title:not([align="right"])')).map((title)=>{
+                return title.querySelector('a').innerText
+            })
+        })
+        console.log('I am here', n)
+        pageTitles = [...pageTitles, ...stories]
+        n++
+    }
+    console.log(pageTitles)
+    await browser.close()
 })()
