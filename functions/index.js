8a41459783f51425130a59c4010358b14405f1fb
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-22 17:55:02 +0200

    added firebase functionality

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-
+const puppeteer = require('puppeteer')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,5 +10,15 @@ const admin = require('firebase-admin')
 // });
 
 exports.addMessage = functions.https.onRequest(async(req, res)=>{
-    res.send('Hello world from firebase').status(200)
+    //res.send('Hello world from firebase').status(200)
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+    const page = await browser.newPage()
+
+    await page.goto('https://news.ycombinator.com/')
+    await page.waitFor('.hnname')
+    const pageTitle = await page.evaluate(()=> document.querySelector('.hnname'))
+
+    res.send(pageTitle)
+    browser.close()
+
 })
