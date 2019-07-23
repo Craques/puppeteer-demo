57a8fb86f4bcd18fedcf9e6eab32c4f9a07b8ad9
Charles Muzhanye <charles@Charless-iMac.local>
2019-07-23 15:54:59 +0200

    added a static build for the frontend

---
+++ build/precache-manifest.48323046cfa9dc94026f4c11d677c979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966105a38ee492a019c90d8562c12b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bbdf7c0de6e17fe611b",
+    "url": "/static/css/main.8c088de9.chunk.css"
+  },
+  {
+    "revision": "28151d8365501bd34a7e",
+    "url": "/static/js/2.fef5f10b.chunk.js"
+  },
+  {
+    "revision": "2bbdf7c0de6e17fe611b",
+    "url": "/static/js/main.3783fd30.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
